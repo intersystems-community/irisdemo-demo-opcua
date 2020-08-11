b1a2f746378cb5c6119adcad8ebcdc57c5f434fe
Amir Samary <amirsamary@gmail.com>
2020-08-11 16:02:42 -0400

    Changing docker autobuild to build the certified server with our iris image

--- build.sh
+++ build.sh
@@ -3,4 +3,10 @@
 DOCKER_REPO=intersystemsdc/irisdemo-demo-opcua
 VERSION=`cat ./VERSION`
 
-docker build -t ${DOCKER_REPO}:version-${VERSION} .
+./generate_certificates.sh
+
+# Building the IRIS Image
+docker build -t ${DOCKER_REPO}:iris-version-${VERSION} ./image-iris
+
+# Building the OPCUA Certified Server Image
+docker build -t ${DOCKER_REPO}:server-ctt-version-${VERSION} ./image-server-ctt
