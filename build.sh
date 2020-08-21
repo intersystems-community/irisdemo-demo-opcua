32e9eaa3a163219106214ea71de32931331ad148
MC <email>
2020-08-21 11:32:47 -0400

    Functional security example

--- build.sh
+++ build.sh
@@ -4,9 +4,17 @@ DOCKER_REPO=intersystemsdc/irisdemo-demo-opcua
 VERSION=`cat ./VERSION`
 
 ./generate_certificates.sh
+if [ "$?" -eq "0" ]; then
+    echo "proceeding with newly generated certficates/credentials..."
+else 
+    echo "skipping certificate/credential (re)generation..."
+fi
+
+set -e
+
+# Building the OPCUA Certified Server Image
+docker build -t ${DOCKER_REPO}:server-ctt-version-${VERSION} ./image-opcua-certified-server
 
 # Building the IRIS Image
 docker build -t ${DOCKER_REPO}:iris-version-${VERSION} ./image-iris
 
-# Building the OPCUA Certified Server Image
-docker build -t ${DOCKER_REPO}:server-ctt-version-${VERSION} ./image-server-ctt
--- generate_certificates.sh
+++ generate_certificates.sh
@@ -1,3 +1,54 @@
 #!/bin/bash
 
-# TODO:
+
+echo "generating certificates/credentials..."
+
+curdir=$(pwd)
+cd certgen
+
+bash certgen.bash
+if [ "$?" -ne "0" ]; then
+
+    set -e
+
+    cd $curdir
+
+    echo "unable to generate new certificates/credentials."
+    exit 1
+
+else
+
+    set -e
+
+    cd $curdir
+
+    echo "finished generating certificates/credentials; copying results..."
+
+    dir=certgen/temp
+
+    server_image_certs_dir=image-opcua-certified-server/certs
+    iris_image_certs_dir=image-iris/certs
+
+    mkdir -p $server_image_certs_dir
+    mkdir -p $iris_image_certs_dir
+
+    svr=secsvr
+    uac=secuac
+    ca=myCA
+
+    cp  $dir/$svr.crt.der   $server_image_certs_dir/$svr.crt.der
+    cp  $dir/$svr.key.der   $server_image_certs_dir/$svr.key.der
+    cp  $dir/$ca.crt.der    $server_image_certs_dir/$ca.crt.der
+    cp  $dir/$ca.crl        $server_image_certs_dir/$ca.crl
+
+    cp  $dir/$uac.crt.der   $iris_image_certs_dir/$uac.crt.der
+    cp  $dir/$uac.key.der   $iris_image_certs_dir/$uac.key.der
+    cp  $dir/$ca.crt.der    $iris_image_certs_dir/$ca.crt.der
+    cp  $dir/$ca.crl        $iris_image_certs_dir/$ca.crl
+
+    echo "done."
+    exit 0
+
+fi
+
+
